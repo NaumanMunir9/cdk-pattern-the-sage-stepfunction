852c31adf78e34fe7c2642cff98ec71ba2c42c20
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-03 09:24:02 +0500

    Initial commit

---
+++ test/the-sage-stepfunction.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TheSageStepfunction from '../lib/the-sage-stepfunction-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/the-sage-stepfunction-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TheSageStepfunction.TheSageStepfunctionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
